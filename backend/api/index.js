4f5c66988ef62bdf199b08d018da5f370bd72763
masifcoder <masifcoder@gmail.com>
2025-03-19 09:54:50 +0500

    new changes 1

--- backend/api/index.js
+++ backend/api/index.js
@@ -11,7 +11,7 @@ const dashboardRouter = require('./routes/admin/dashboard.routes');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
@@ -36,9 +36,9 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     // Start server after successful database connection
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
+    // app.listen(port, () => {
+    //   console.log(`Server is running on port ${port}`);
+    // });
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
